7d7a1de2e71f95559cff5add3c1da49842e23791
Piotr Mitros <piotr@mitros.org>
2019-10-11 19:38:15 +0100

    Web sockets!

--- webapp/aio_webapp.py
+++ webapp/aio_webapp.py
@@ -1,6 +1,7 @@
 import time
 
 import asyncio
+import aiohttp
 from aiohttp import web
 import aiohttp_cors
 from aiohttp.web import middleware
@@ -31,9 +32,30 @@ async def hello(request):
     print(server_data)
     return web.Response(text="Acknowledged!")
 
+async def websocket_handler(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            print(msg)
+            if msg.data == 'close':
+                await ws.close()
+            else:
+                await ws.send_str(msg.data + '/answer')
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            print('ws connection closed with exception %s' %
+                  ws.exception())
+
+    print('websocket connection closed')
+
+    return ws
+
 app = web.Application()
 
 app.on_response_prepare.append(debug_signal)
+
+app.add_routes([web.get('/wsapi/', websocket_handler)])
 
 app.add_routes([
     web.get('/webapi/', hello),
