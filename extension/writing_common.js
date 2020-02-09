600d8aedc8c4be7475a4d97f45f8bd45e4843d9b
Piotr Mitros <piotr@mitros.org>
2020-02-08 21:01:49 -0500

    Moving all AJAX into the background page

--- extension/writing_common.js
+++ extension/writing_common.js
@@ -1,32 +1,3 @@
-var WRITINGJS_AJAX_SERVER = "https://writing.hopto.org/webapi/";
-var WRITINGJS_WSS_SERVER = "https://writing.hopto.org/webapi/";
-var EXPERIMENTAL_WEBSOCKET = false;
-
-
-chrome.storage.sync.get(['process-server'], function(result) {
-    var WRITINGJS_AJAX_SERVER = result['process-server'];
-    if(!WRITINGJS_AJAX_SERVER) {
-	WRITINGJS_AJAX_SERVER = "https://writing.hopto.org/webapi/";
-    }
-});
-
-function writingjs_ajax(data) {
-    /*
-      Helper function to send a logging AJAX request to the server.
-      This function takes a JSON dictionary of data.
-
-      TODO: Convert to a queue for offline operation using Chrome
-      Storage API? Cache to Chrome Storage? Chrome Storage doesn't
-      support meaningful concurrency,
-     */
-
-    httpRequest = new XMLHttpRequest();
-    //httpRequest.withCredentials = true;
-    httpRequest.open("POST", WRITINGJS_AJAX_SERVER);
-    httpRequest.send(JSON.stringify(data));
-}
-
-
 function googledocs_id_from_url(url) {
     /*
       Given a URL like:
