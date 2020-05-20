c98e5d5ddf7bebf62ec01492fe3a9ac966a825a3
Piotr Mitros <piotr@mitros.org>
2020-05-20 22:37:31 +0000

    Restream can now rate-throttle

--- webapp/restream.py
+++ webapp/restream.py
@@ -2,11 +2,13 @@
 ReStream
 
 Usage:
-    restream.py [--url=<url>] [--extract-client] <filename>
+    restream.py [--url=<url>] [--extract-client] [--rate=<rate>] [--max-wait=<sec>] <filename>
 
 Options
     --url=<url>       URL to connect [default: http://localhost:8888/]
     --extract-client  Parse JSON and extract client-side event
+    --rate=<rate>     Throttle events to: rate*timestamps
+    --max-wait=<sec>  Maximum delay (if throttling)
 
 Overview:
     * Restream logs from a file a web sockets server
@@ -23,6 +25,8 @@ import aiofiles
 import aiohttp
 import docopt
 
+print(docopt.docopt(__doc__))
+
 
 async def run():
     '''
@@ -36,15 +40,26 @@ async def run():
     '''
     args = docopt.docopt(__doc__)
     print(args['--extract-client'])
+    old_ts = None
 
     async with aiohttp.ClientSession() as session:
         async with session.ws_connect(args["--url"]) as web_socket:
             async with aiofiles.open(args['<filename>']) as log_file:
                 async for line in log_file:
+                    if args["--rate"] is not None:
+                        new_ts = json.loads(line)["server"]["time"]
+                        if old_ts is not None:
+                            rate = float(args["--rate"])
+                            delay = (new_ts-old_ts)*rate
+                            if args["--max-wait"] is not None:
+                                delay = min(delay, float(args["--max-wait"]))
+                            print(line)
+                            print(delay)
+                            await asyncio.sleep(delay)
+                        old_ts = new_ts
                     if args['--extract-client']:
                         line = json.dumps(json.loads(line)['client'])
                     await web_socket.send_str(line.strip())
-
 try:
     asyncio.run(run())
 except aiohttp.client_exceptions.ServerDisconnectedError:
