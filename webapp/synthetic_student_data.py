113f2b540ceca22f3126464b699771311ce83e98
Piotr Mitros <piotr@mitros.org>
2020-05-20 20:15:16 +0000

    We can create synthetic data for the UX

---
+++ webapp/synthetic_student_data.py
@@ -0,0 +1,64 @@
+'''
+Note that current loremipsum in `pip` is not Python 3
+compatible. If you are getting b'' in your text, the
+patch is at:
+
+`https://github.com/monkeython/loremipsum/issues/10`
+'''
+
+import json
+import random
+import math
+
+import numpy
+import numpy.random
+
+import loremipsum
+import names
+
+
+def paginate(l, n):
+    '''
+    Paginate list `l` into `n`-item rows.
+
+    This should move into the client
+    '''
+    return [l[i*n:(i+1)*n] for i in range(math.ceil(len(l)/n))]
+
+
+def synthetic_student_data(student_id):
+    name = names.get_first_name()
+    essay = "\n".join(loremipsum.get_paragraphs(5))
+    return {
+        'id': student_id,
+        'name': name,
+        'email': "{name}@school.district.us".format(name=name),
+        'address': "1 Main St",
+        'phone': "({pre})-{mid}-{post}".format(
+            pre=random.randint(200, 999),
+            mid=random.randint(200, 999),
+            post=random.randint(1000, 9999)),
+        'avatar': "avatar-{number}".format(number=random.randint(0,14)),
+        'ici': random.uniform(100, 1000),
+        'essay_length': len(essay),
+        'essay': essay,
+        'writing_time': random.uniform(5, 60),
+        'text_complexity': random.uniform(3,9),
+        'google_doc': "https://docs.google.com/document/d/1YbtJGn7ida2IYNgwCFk3SjhsZ0ztpG5bMzA3WNbVNhU/edit",
+        'time_idle': numpy.random.gamma(0.5,scale=5),
+        'outline': [{"section": "Problem "+str(i+1),
+                     "length": random.randint(1,300)} for i in range(5)],
+        'revisions': {}
+    }
+
+
+def synthetic_data(n=20):
+    data=[
+        synthetic_student_data(n)
+        for i in range(n)
+    ]
+    return paginate(data, 4)
+
+
+if __name__ == '__main__':
+    print(synthetic_data())
