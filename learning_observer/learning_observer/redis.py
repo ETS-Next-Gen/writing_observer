b7081d3e1d7edbe0c139b9bf5b1c5abb3e97a417
Piotr Mitros <piotr@mitros.org>
2021-08-17 19:57:31 -0400

    Missing commit: redis.py

---
+++ learning_observer/learning_observer/redis.py
@@ -0,0 +1,29 @@
+'''
+Little helper for per-thread connection pooling. We want just one
+redis connection.
+'''
+
+import asyncio_redis
+
+redis_connection = None
+
+async def connect():
+    '''
+    Connect to redis
+    '''
+    global redis_connection
+    print(redis_connection)
+    if redis_connection is None:
+        redis_connection = await asyncio_redis.Connection.create()
+        print(redis_connection)
+
+
+async def connection():
+    '''
+    Returns our connection. Connects if needed.
+
+    This is shorthand. It's not clear if this is the right abstraction,
+    since it makes for a mess of awaits.
+    '''
+    connect()
+    return redis_connection()
