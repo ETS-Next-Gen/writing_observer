d0a8d2c135a92cd7b1a17a611a9c219f9d226191
DrLynch <cflynch@ncsu.edu>
2022-11-16 15:40:00 -0500

    Added in missing documentation files from prior fork and added in scripts that are built to support execution/backup on the server process.

---
+++ servermanagement/RunLearningObserver.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# ===============================
+# RunLearningObserver.sh
+# Collin F. Lynch
+#
+# This bash script provides a simple wrapper to run the 
+# learning observer service and pipe the data to a logfile
+# over time this should be integrated into the systemd 
+# service process.  This uses static variables to specify
+# the location of the virtualenv and the command and 
+# specifies the location for the running logfile. 
+
+# System Variables
+# --------------------------------------
+VIRTUALENV_PYTHON="/usr/local/share/Projects/WritingObserver/VirtualENVs/learning_observer/bin/python3.8"
+LEARNING_OBSERVER_LOC="/usr/local/share/Projects/WritingObserver/Repo-Fork/writing_observer/learning_observer"
+LOGFILE_DEST="/usr/local/share/Projects/WritingObserver/Repo-Fork/writing_observer/learning_observer/learning_observer/logs"
+
+# Make the logfile name
+# ---------------------------------------
+LOG_DATE=$(date "+%m-%d-%Y--%H-%M-%S")
+LOGFILE_NAME="$LOGFILE_DEST/learning_observer_service_$LOG_DATE.log"
+echo $LOG_NAME;
+
+ 
+# Now run the thing.
+# --------------------------------------
+echo "Running Learning Observer Service..."
+cd $LEARNING_OBSERVER_LOC
+#$($VIRTUALENV_PYTHON $LEARNING_OBSERVER_LOC > $LOG_NAME 2>&1)
+nohup $VIRTUALENV_PYTHON learning_observer > $LOGFILE_NAME 2>&1 &
+PROCESS_ID=$!
+echo $PROCESS_ID > $LOGFILE_DEST/run.pid
+
+
+# Set the number of allowed open files to something large 8192
+prlimit --pid $PROCESS_ID --nofile=8192
