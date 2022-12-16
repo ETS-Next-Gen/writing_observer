1726ddfc6faa26d9c4da497dd170232d1bdccb84
DrLynch <cflynch@lin-res122.csc.ncsu.edu>
2022-12-16 01:01:22 -0500

    Adding basic cleanup to roster.py to fix the camel-case/vs underscore inconsistency and adding a hack to address the naming conventions for the IDs from google users.

--- learning_observer/learning_observer/rosters.py
+++ learning_observer/learning_observer/rosters.py
@@ -93,6 +93,8 @@ def clean_google_ajax_data(resp_json, key, sort_key, default=None, source=None):
     - We often want some default if that field is missing (`default`)
     - We often want the response sensibly sorted (`sort_key`)
     '''
+    print(">> RESP_JSON_IN:", key, resp_json)
+
     # Convert errors into appropriate codes for clients
     # Typically, resp_json['error'] == 'UNAUTHENTICATED'
     if 'error' in resp_json:
@@ -122,7 +124,9 @@ def clean_google_ajax_data(resp_json, key, sort_key, default=None, source=None):
     # Sort the list
     if sort_key is not None:
         resp_json.sort(key=sort_key)
-
+  
+        
+    print("<< RESP_JSON_OUT:", resp_json)
     return resp_json
 
 
