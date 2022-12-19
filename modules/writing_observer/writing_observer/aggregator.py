4e911786d86f23a5a021bb5ec3038c96261e5c72
DrLynch <cflynch@ncsu.edu>
2022-12-19 00:27:35 -0500

    Updated with upstream content and removed local debugging printing.

--- modules/writing_observer/writing_observer/aggregator.py
+++ modules/writing_observer/writing_observer/aggregator.py
@@ -195,12 +195,10 @@ async def retrieve_latest_documents_kvs(student_data):
             KeyStateType.INTERNAL
         ) for s in active_students]) # in student_data if 'writing_observer.writing_analysis.last_document' in s])
 
-    print(">>> DOC KEYS")
     print(document_keys)
     
     kvs_data = await kvs.multiget(keys=document_keys)
 
-    print(">> WRITING DATA: KVS Data", kvs_data)
     
     # Return blank entries if no data, rather than None. This makes it possible
     # to use item.get with defaults sanely.
