fe0c44a3fb3a042cbc1c1cda6f5df7bcc94e3e37
DrLynch <cflynch@ncsu.edu>
2022-12-19 00:27:35 -0500

    Updated with upstream content and removed local debugging printing.

--- learning_observer/learning_observer/auth/social_sso.py
+++ learning_observer/learning_observer/auth/social_sso.py
@@ -275,8 +275,6 @@ async def _google(request):
         async with client.post(url, data=params) as resp:
             data = await resp.json()
 
-        # print(">>>>DATA::")
-        # print(data)
         assert 'access_token' in data, data
 
         # get user profile
