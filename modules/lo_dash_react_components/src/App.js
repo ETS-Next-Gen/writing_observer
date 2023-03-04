8c826cd13d34882738207473834312e41d818a43
Brad Erickson <bbwe24@gmail.com>
2023-03-03 22:14:05 -0500

    finished header component and cleaned up app.js

--- modules/lo_dash_react_components/src/App.js
+++ modules/lo_dash_react_components/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -12,85 +12,113 @@ import {
   StudentSelectHeader,
 } from "./lib";
 
-function App() {
-  const defaultProps = {
-    students: ["Bart", "Lisa", "Milhouse"],
-    selected: 'Bart',
-    label: "Yellow!"
-  };
-  const [state, setState] = useState(() => {
-    const initialState = {};
-    Object.entries(StudentSelectHeader.propTypes)
-      .filter(([prop, value]) => prop !== "setProps")
-      .forEach(([prop, value]) => {
-        initialState[prop] = value.isRequired ? null : undefined;
-        if (defaultProps.hasOwnProperty(prop)) {
-          initialState[prop] = defaultProps[prop];
-        }
-      });
-    // console.log(initialState);
-    return initialState;
-  });
-  const setProps = (newProps) => {
-    setState((prevState) => {
-      return { ...prevState, ...newProps };
-    });
-  };
-  return (
-    <div className="App">
-      <StudentSelectHeader
-        students={["Bart", "Lisa", "Milhouse"]}
-        selected="Bart"
-        setProps={setProps}
-      />
-      {/* <LOConnection /> */}
-      <LOMetrics
-        data={{
-          sentences: {
-            id: "sentences",
-            value: 33,
-            label: " sentences",
-          },
-          paragraphs: {
-            id: "paragraphs",
-            value: 45,
-            label: " paragraphs",
-          },
-        }}
-        shown={["paragraphs", "sentences"]}
-      />
-      <LOIndicatorBars
-        data={{
-          sentences: {
-            id: "sentences",
-            value: 33,
-            label: " bananas",
-          },
-          paragraphs: {
-            id: "paragraphs",
-            value: 45,
-            label: " paragraphs",
-          },
-        }}
-        shown={["paragraphs", "sentences"]}
-      />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selected: "Bart",
+    };
+    this.setProps = this.setProps.bind(this);
+  }
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editing <code>src/App.js</code> and save to reload and this changes.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  setProps(newProps) {
+    this.setState(newProps);
+  }
+
+  render() {
+    return (
+      <div>
+        <StudentSelectHeader
+          students={["Bart", "Lisa", "Milhouse"]}
+          selected="Bart"
+          setProps={this.setProps}
+          {...this.state}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
+// function App() {
+//   const defaultProps = {
+//     students: ["Bart", "Lisa", "Milhouse"],
+//     selected: 'Bart',
+//     label: "Yellow!"
+//   };
+//   const [state, setState] = useState(() => {
+//     const initialState = {};
+//     Object.entries(StudentSelectHeader.propTypes)
+//       .filter(([prop, value]) => prop !== "setProps")
+//       .forEach(([prop, value]) => {
+//         initialState[prop] = value.isRequired ? null : undefined;
+//         if (defaultProps.hasOwnProperty(prop)) {
+//           initialState[prop] = defaultProps[prop];
+//         }
+//       });
+//     // console.log(initialState);
+//     return initialState;
+//   });
+//   const setProps = (newProps) => {
+//     setState((prevState) => {
+//       return { ...prevState, ...newProps };
+//     });
+//   };
+//   return (
+//     <div className="App">
+//       <StudentSelectHeader
+//         students={["Bart", "Lisa", "Milhouse"]}
+//         selected="Bart"
+//         setProps={setProps}
+//       />
+//       {/* <LOConnection /> */}
+//       <LOMetrics
+//         data={{
+//           sentences: {
+//             id: "sentences",
+//             value: 33,
+//             label: " sentences",
+//           },
+//           paragraphs: {
+//             id: "paragraphs",
+//             value: 45,
+//             label: " paragraphs",
+//           },
+//         }}
+//         shown={["paragraphs", "sentences"]}
+//       />
+//       <LOIndicatorBars
+//         data={{
+//           sentences: {
+//             id: "sentences",
+//             value: 33,
+//             label: " bananas",
+//           },
+//           paragraphs: {
+//             id: "paragraphs",
+//             value: 45,
+//             label: " paragraphs",
+//           },
+//         }}
+//         shown={["paragraphs", "sentences"]}
+//       />
+
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Editing <code>src/App.js</code> and save to reload and this changes.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- modules/lo_dash_react_components/src/lib/components/StudentSelectHeader.react.js
+++ modules/lo_dash_react_components/src/lib/components/StudentSelectHeader.react.js
@@ -1,87 +1,131 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import classnames from "classnames";
 
 /**
+ * Displays a header menu for selecting students
  */
 export default class StudentSelectHeader extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            showDropdown: false,
-            selected: props.selected
-        }
-    }
-
-    handleClick() {
-        this.setState({ showDropdown: !this.state.showDropdown })
+  /**
+   * Constructor for the StudentSelectHeader component.
+   *
+   * @param {object} props - The props for the component.
+   */
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDropdown: false,
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.iterateName = this.iterateName.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+  }
 
-    handleSelect(name) {
-        this.props.setProps({selected: name});
-        this.setState({ showDropdown: false, selected: name })
-    };
+  /**
+   * Toggles the visibility of the dropdown menu when the user clicks on the student name.
+   */
+  handleClick() {
+    this.setState((prevState) => ({ showDropdown: !prevState.showDropdown }));
+  }
 
-    render() {
-        const {id, class_name, students, selected} = this.props;
-        const {showDropdown} = this.state;
-        console.log(students, selected);
-        console.log(this.state.selected);
-    
-        return (
-            <header id='student-select-header' className="nav-header">
-                <div className="button-left">
-                    <button className="button">&lt;</button>
-                </div>
-                <div className="header-student" onClick={() => this.handleClick()}>{this.state.selected}</div>
-                {showDropdown && (
-                    <ul className="dropdown">
-                        {students.map((name) => (
-                            <li
-                                key={name}
-                                onClick={() => this.handleSelect(name)}
-                                className={
-                                name === this.state.selected
-                                    ? 'dropdown-item dropdown-item-selected'
-                                    : 'dropdown-item'
-                                }
-                            >
-                                {name}
-                            </li>
-                        ))}
-                    </ul>
-                )}
-                <div className="button-right">
-                    <button className="button">&gt;</button>
-                </div>
-            </header>
-        );
-    }
+  /**
+   * Iterates through the list of students and updates the selected student accordingly.
+   *
+   * @param {number} offset - The offset to apply to the current student index.
+   */
+  iterateName(offset) {
+    const { students, selected, setProps } = this.props;
+    const curr_index = students.indexOf(selected);
+    const new_name =
+      students[
+        curr_index + offset >= 0
+          ? (curr_index + offset) % students.length
+          : students.length - 1
+      ];
+    setProps({ selected: new_name });
+  }
+
+  /**
+   * Handles the user selecting a new student from the dropdown menu.
+   *
+   * @param {string} name - The name of the selected student.
+   */
+  handleSelect(name) {
+    const { setProps } = this.props;
+    setProps({ selected: name });
+    this.setState({ showDropdown: false });
+  }
+
+  /**
+   * Renders the StudentSelectHeader component.
+   *
+   * @returns {JSX.Element} - The rendered component.
+   */
+  render() {
+    const { id, className, students, selected } = this.props;
+    const { showDropdown } = this.state;
+    const classNames = classnames("student-select-header", className);
+
+    return (
+      <div id={id} className={classNames}>
+        <header id="student-select-header" className="nav-header">
+          <div className="button-left">
+            <button className="button" onClick={() => this.iterateName(-1)}>
+              &lt;
+            </button>
+          </div>
+          <div className="header-student" onClick={this.handleClick}>
+            {selected}
+          </div>
+          {showDropdown && (
+            <ul className="dropdown">
+              {students.map((name) => (
+                <li
+                  key={name}
+                  onClick={() => this.handleSelect(name)}
+                  className={
+                    name === selected
+                      ? "dropdown-item dropdown-item-selected"
+                      : "dropdown-item"
+                  }
+                >
+                  {name}
+                </li>
+              ))}
+            </ul>
+          )}
+          <div className="button-right">
+            <button className="button" onClick={() => this.iterateName(1)}>
+              &gt;
+            </button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 StudentSelectHeader.propTypes = {
-    /**
-     * The ID used to identify this component in Dash callbacks.
-     */
-    id: PropTypes.string,
+  /** A unique identifier for the component, used to identify it in Dash callbacks */
+  id: PropTypes.string,
 
-    /**
-     * Classes for the outer most div.
-     */
-    class_name: PropTypes.string,
+  /** A string of class names to be added to the outermost div */
+  className: PropTypes.string,
 
-    /**
-     * Classes for the outer most div.
-     */
-    students: PropTypes.array,
+  /** An array of student names to be displayed in the dropdown */
+  students: PropTypes.arrayOf(PropTypes.string),
 
-    /**
-     * Classes for the outer most div.
-     */
-    selected: PropTypes.string,
+  /** The currently selected student name */
+  selected: PropTypes.string,
 
-    /**
-     * Classes for the outer most div.
-     */
-    setProps: PropTypes.func
-}
+  /**
+   * Dash-assigned callback that should be called to report property changes
+   * to Dash, to make them available for callbacks.
+   */
+  setProps: PropTypes.func,
+};
+
+StudentSelectHeader.defaultProps = {
+  students: [],
+  selected: "",
+};
