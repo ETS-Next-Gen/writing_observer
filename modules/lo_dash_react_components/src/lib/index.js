7e1dc861606e90f2c40614cb7ae039e5f115e3ac
Brad Erickson <bbwe24@gmail.com>
2023-03-07 12:34:38 -0500

    renamed lotexthighlight to wotexthighlight

--- modules/lo_dash_react_components/src/lib/index.js
+++ modules/lo_dash_react_components/src/lib/index.js
@@ -1,7 +1,7 @@
 import LOConnection from './components/LOConnection.react';
 import WOMetrics from './components/WOMetrics.react';
 import WOIndicatorBars from './components/WOIndicatorBars.react';
-import LOTextHighlight from './components/LOTextHighlight.react';
+import LOTextHighlight from './components/WOTextHighlight.react';
 import StudentSelectHeader from './components/StudentSelectHeader.react';
 import LOTextMinibars from './components/LOTextMinibars.react';
 
