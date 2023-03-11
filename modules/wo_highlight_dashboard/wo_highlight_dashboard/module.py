e308fc7f3113eec73231a8d860388a604f3c7f42
DrLynch <cflynch@ncsu.edu>
2023-03-10 22:37:47 -0500

    Additional Teak.

--- modules/wo_highlight_dashboard/wo_highlight_dashboard/module.py
+++ modules/wo_highlight_dashboard/wo_highlight_dashboard/module.py
@@ -47,8 +47,7 @@ THIRD_PARTY = {
     },
     "css/fontawesome_all.css": {
         "url": dbc.icons.FONT_AWESOME,
-        "hash":
-        "1496214e7421773324f4b332127ea77bec822fc6739292ebb19c6abcc22a5"
+        "hash": "1496214e7421773324f4b332127ea77bec822fc6739292ebb19c6abcc22a5"
         "6248e0634b4e0ca0c2fcac14dc10b8d01fa17febaa35f46731201d1ffd0ab482dd7"
         
     },
