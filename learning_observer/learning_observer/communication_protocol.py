7ad84cedaded19e3b2389b495ebc016c2956e7d9
Bradley Erickson <bbwe24@gmail.com>
2023-04-10 14:36:00 -0400

    I forgot an untracked file in the initial commit

---
+++ learning_observer/learning_observer/communication_protocol.py
@@ -0,0 +1,17 @@
+'''
+This file defines the base schema for the client/server
+communication protocol.
+
+When a new message is received from the client, we validate
+it against the module specific schema (or the base schema when
+one is not found for a given module) using jsonschema
+'''
+
+base_schema = {
+    'type': 'object',
+    'properties': {
+        'user_id': {'type': 'string'},
+        'timestamp': {'type': 'string', 'format': 'date-time'}
+    },
+    'required': ['user_id', 'timestamp'],
+}
