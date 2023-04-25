754b940fcff5cdd53534e95928758f4cd6635f33
Bradley Erickson <bbwe24@gmail.com>
2023-04-25 12:19:37 -0400

    added comments and docstrings to code

--- learning_observer/learning_observer/pipeline.py
+++ learning_observer/learning_observer/pipeline.py
@@ -6,7 +6,13 @@ import re
 import sys
 
 
-def parse_dot(dot):
+def parse_dot(dot: str) -> dict:
+    """
+    Parses the given DOT notation and returns a dictionary of nodes and their dependencies.
+
+    :param dot: (str) DOT notation string defining the pipeline of functions and their dependencies.
+    :return: (dict) Dictionary containing nodes and their dependencies.
+    """
     nodes = re.findall(r'(\w+)\s*->\s*(\w+)', dot)
     graph = collections.defaultdict(list)
 
@@ -16,7 +22,13 @@ def parse_dot(dot):
     return graph
 
 
-def build_level_mapping(graph):
+def build_level_mapping(graph: dict) -> dict:
+    """
+    Takes a dictionary of nodes and their dependencies and returns a dictionary of nodes grouped by their level in the dependency tree.
+
+    :param graph: (dict) Dictionary containing nodes and their dependencies.
+    :return: (dict) Dictionary containing nodes grouped by their level in the dependency tree.
+    """
     levels = {}
     visited = set()
 
@@ -37,7 +49,13 @@ def build_level_mapping(graph):
     return levels
 
 
-def create_parent_mapping(graph):
+def create_parent_mapping(graph: dict) -> dict:
+    """
+    Takes a dictionary of nodes and their dependencies and returns a dictionary of nodes mapped to their parents.
+
+    :param graph: (dict) Dictionary containing nodes and their dependencies.
+    :return: (dict) Dictionary containing nodes mapped to their parents.
+    """
     reversed_graph = {}
     for parent, children in graph.items():
         for child in children:
@@ -47,8 +65,20 @@ def create_parent_mapping(graph):
     return reversed_graph
 
 
-async def process_pipeline(dot, params, runtime, sd):
+# TODO adjust runtime to expect the runtime object, just hacking so a dict is fine
+async def process_pipeline(dot: str, params: dict, runtime: dict, sd: dict) -> dict:
+    """
+    Processes the pipeline of functions defined by the DOT notation and returns a dictionary of results for each node.
 
+    :param dot: (str) DOT notation string defining the pipeline of functions and their dependencies.
+    :param params: (dict) Dictionary containing input parameters for each node in the pipeline.
+    :param runtime: (dict) Dictionary containing runtime parameters for the pipeline functions.
+    :param sd: (dict) Dictionary containing student data to be used by the pipeline functions.
+    :return: (dict) Dictionary containing the output result of each function in the pipeline.
+    """
+
+    # TODO fetch the pipeline functions, this is something we will need to create
+    # within the module_loader
     # function_mapping = learning_observer.module_loader.pipeline_functions()
     function_mapping = {
         'A': func_a,
@@ -59,14 +89,21 @@ async def process_pipeline(dot, params, runtime, sd):
         'F': func_F
     }
 
-    def prep_function_for_pipeline(node, parent_output=None):
+    def prep_function_for_pipeline(node: str, parent_output: dict = None) -> callable:
+        """
+        Takes a node and its dependencies, and returns an async function that can be run in the pipeline.
+
+        :param node: (str) Node name to be processed.
+        :param parent_output: (dict) Dictionary containing output results from the node's parent nodes.
+        :return: (callable) Async function to be run in the pipeline.
+        """
         func = function_mapping[node]
-        func_params = params.get(node, {})
         args_aggregator = inspect.getfullargspec(func).args
 
+        # handle adding kwargs to function params
+        func_params = params.get(node, {})
         if parent_output is not None and parent_output != {}:
             # TODO determine how we deal with multiple parent outputs
-            print('here', node, ''.join(parent_output.values()))
             func_params.update({'data': ''.join(parent_output.values())})
 
         if 'runtime' in args_aggregator:
@@ -75,10 +112,9 @@ async def process_pipeline(dot, params, runtime, sd):
         if 'student_data' in args_aggregator:
             func_params.update({'student_data': sd})
 
-        # return async function
+        # return the function if its async or make it async
         if inspect.iscoroutinefunction(func):
             return func(**func_params)
-        # wrap sync function in async
         partial_func = functools.partial(func, **func_params)
         loop = asyncio.get_event_loop()
         return loop.run_in_executor(None, partial_func)
@@ -152,11 +188,13 @@ functions = {
 
 # Define the DOT Notation string
 dot_string = """
-  A -> B
-  A -> C
-  B -> D
-  C -> D
-  E -> F
+digraph name {
+  A -> B;
+  A -> C;
+  B -> D;
+  C -> D;
+  E -> F;
+}
 """
 
 parameters = {'A': {'data': 'start_A'}, 'E': {'data': 'start_E'}}
