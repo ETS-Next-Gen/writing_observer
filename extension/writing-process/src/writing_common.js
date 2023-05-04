9367805b9ced5681c5257d6504907fdcefd8edfc
Bradley Erickson <bbwe24@gmail.com>
2023-05-04 10:45:13 -0400

    moved files to new extension build process directory

--- extension/writing-process/src/writing_common.js
+++ extension/writing-process/src/writing_common.js
@@ -52,7 +52,7 @@ function treeget(tree, key) {
 }
 
 
-function googledocs_id_from_url(url) {
+export function googledocs_id_from_url(url) {
     /*
       Given a URL like:
         https://docs.google.com/document/d/jkldfhjklhdkljer8934789468976sduiyui34778dey/edit/foo/bar
