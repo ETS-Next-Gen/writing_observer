bfcab00c296336d91e84d68d2416786b1b806154
Bradley Erickson <bbwe24@gmail.com>
2023-08-10 13:36:06 -0400

    added openai key to creds.yaml and made gpt call non blocking

--- modules/writing_observer/writing_observer/gpt.py
+++ modules/writing_observer/writing_observer/gpt.py
@@ -1,10 +1,15 @@
+import asyncio
+import functools
 import openai
+import os
+from concurrent.futures import ThreadPoolExecutor
 
 import learning_observer.communication_protocol.integration
-
+import learning_observer.settings
 
 model = 'gpt-3.5-turbo-16k'
 template = """{question}\n\n{text}"""
+openai.api_key = learning_observer.settings.module_setting('writing_observer', 'openai_api_key', os.getenv('OPENAI_API_KEY'))
 
 
 @learning_observer.communication_protocol.integration.publish_function('writing_observer.gpt_essay_prompt')
@@ -15,12 +20,13 @@ async def process_student_essay(text, prompt):
     We use a closure to allow the system to connect to the memoization KVS.
     '''
 
+    executor = ThreadPoolExecutor()
+
     @learning_observer.cache.async_memoization()
     async def gpt(gpt_prompt):
-        completion = openai.ChatCompletion.create(
-            model=model,
-            messages=[{"role": "user", "content": gpt_prompt}]
-        )
+        loop = asyncio.get_event_loop()
+        partial = functools.partial(openai.ChatCompletion.create, model=model, messages=[{"role": "user", "content": gpt_prompt}])
+        completion = await loop.run_in_executor(executor, partial)
         return completion["choices"][0]["message"]["content"]
 
     if len(prompt) == 0:
