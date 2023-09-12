c51bfe36fcb2026ba8a4972f656c8696803a2691
Piotr Mitros <piotr@mitros.org>
2023-09-12 14:18:13 -0400

    awe_requirements

--- learning_observer/setup.py
+++ learning_observer/setup.py
@@ -10,10 +10,17 @@ import os
 
 my_path = os.path.dirname(os.path.realpath(__file__))
 parent_path = os.path.abspath(os.path.join(my_path, os.pardir))
-file_path = os.path.join(parent_path, 'requirements.txt')
+req_path = os.path.join(parent_path, 'requirements.txt')
+awe_path = os.path.join(parent_path, 'awe_requirements.txt')
 
-requirements = [s.split("#")[0].strip() for s in open(file_path).readlines() if len(s)>1]
+def clean_requirements(filename):
+    file_path = os.path.join(parent_path, filename)
+    requirements = [s.split("#")[0].strip() for s in open(file_path).readlines() if len(s)>1]
+    return requirements
 
 setup(
-    install_requires = requirements
+    install_requires = clean_requirements(req_path),
+    extras_require = {
+        "awe": clean_requirements(awe_path)
+    }
 )
