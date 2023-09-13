6014f8d082526be2fadcbdc9fa53efbc92e32774
Piotr Mitros <piotr@mitros.org>
2023-09-12 21:42:59 -0400

    wo_common_student_errors copies assets to virtualenv

--- modules/wo_common_student_errors/setup.py
+++ modules/wo_common_student_errors/setup.py
@@ -6,5 +6,8 @@ setup.py develop` or just install via requirements.txt
 from setuptools import setup, find_packages
 
 setup(
-    name="wo_common_student_errors"
+    name="wo_common_student_errors",
+    package_data={
+        'wo_common_student_errors': ['assets/*'],
+    }
 )
