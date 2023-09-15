cb388ddf29c0b5b7ee3878c143655844d7cb1832
Bradley Erickson <bbwe24@gmail.com>
2023-09-15 10:47:12 -0400

    removed hack that didnt work, trying a different possible fix

--- modules/lo_dash_react_components/setup.py
+++ modules/lo_dash_react_components/setup.py
@@ -8,13 +8,6 @@ new_path = '/'.join(sys.executable.split('/')[:-1])
 current_path = os.environ['PATH']
 modified_env = {'PATH': f'{new_path}{os.pathsep}{current_path}'}
 
-# HACK we need dash installed before we run the NPM commands
-# which transform our React components into a Python package
-try:
-    import dash
-except ImportError:
-    subprocess.run([sys.executable, '-m', 'pip', 'install', 'dash[dev]'])
-
 # TODO make sure the user is using a supported version Node
 subprocess.run(['npm', 'install', os.path.abspath(os.path.dirname(__file__))], env=modified_env)
 subprocess.run(['npm', 'run', '--prefix', os.path.abspath(os.path.dirname(__file__)), 'build'], env=modified_env)
@@ -32,7 +25,7 @@ setup(
     include_package_data=True,
     license=package['license'],
     description=package.get('description', package_name),
-    install_requires=[],
+    install_requires=['dash'],
     classifiers=[
         'Framework :: Dash',
     ],
