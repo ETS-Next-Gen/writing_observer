66687c73cd8f4a7a2c4978658624c86f303748c0
Bradley Erickson <bbwe24@gmail.com>
2023-10-02 16:48:21 -0400

    woops forgot the config file for bundling

---
+++ extension/writing-process/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        background: path.join(__dirname, 'src', 'background.js'),
+        service_worker: path.join(__dirname, 'src', 'service_worker.js'),
+        writing_common: path.join(__dirname, 'src', 'writing_common.js'),
+        writing: path.join(__dirname, 'src', 'writing.js'),
+        inject: path.join(__dirname, 'src', 'inject.js'),
+    },
+    output: {
+        path: path.join(__dirname, 'src'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader'],
+            }
+        ],
+    },
+};
