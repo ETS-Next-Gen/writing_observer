e9642680cbc1f8b3991c22e60fe8991a0d4b8e21
Piotr Mitros <piotr@mitros.org>
2023-12-02 19:49:23 -0500

    lo_event comments

--- modules/lo_event/lo_event/lo_event.js
+++ modules/lo_event/lo_event/lo_event.js
@@ -64,6 +64,14 @@ async function initializeLoggers () {
  *
  * This is useful for items such as `source` and `version`
  * which should be the same for every event.
+ *
+ * Note that at the time this was written, we did not consider the use
+ * case of using setFieldSet after INIT_READY. This may or may not
+ * work, but has complex issues with consistency and async (e.g. if
+ * setFieldSet executes after a send event) and especially with
+ * disconnects (e.g. will the field set be configured for the right
+ * events). At some point, this should probably be made into a queued
+ * event, and reconnection issues should be figured out.
  */
 export function setFieldSet (data) {
   currentState = currentState.then(
