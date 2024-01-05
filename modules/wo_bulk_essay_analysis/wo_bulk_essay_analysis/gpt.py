e418a4a7d1d05db0d3667a3f9b98fdf20c9573e2
Bradley Erickson <bbwe24@gmail.com>
2024-01-05 09:34:29 -0500

    upgrade openai to new version and put debug message behind else

--- modules/wo_bulk_essay_analysis/wo_bulk_essay_analysis/gpt.py
+++ modules/wo_bulk_essay_analysis/wo_bulk_essay_analysis/gpt.py
@@ -1,8 +1,5 @@
-import asyncio
-import functools
-import openai
 import os
-from concurrent.futures import ThreadPoolExecutor
+from openai import AsyncOpenAI, OpenAIError
 
 import learning_observer.communication_protocol.integration
 import learning_observer.prestartup
@@ -25,22 +22,25 @@ class OpenAIGPT(GPTAPI):
     def __init__(self, model):
         super().__init__()
         self.model = model
-        openai.api_key = learning_observer.settings.module_setting('writing_observer', 'openai_api_key', os.getenv('OPENAI_API_KEY'))
+        self.client = AsyncOpenAI(api_key=learning_observer.settings.module_setting('writing_observer', 'openai_api_key', os.getenv('OPENAI_API_KEY')))
 
-    def chat_completion(self, prompt, system_prompt):
+    async def chat_completion(self, prompt, system_prompt):
         messages = [
             {'role': 'system', 'content': system_prompt},
             {'role': 'user', 'content': prompt}
         ]
-        return openai.ChatCompletion.create(model=self.model, messages=messages)
+        return await self.client.chat.completions.create(model=self.model, messages=messages)
 
 
 @learning_observer.prestartup.register_startup_check
 def initialize_gpt_responder():
     global gpt_responder
-    gpt_responder = OpenAIGPT('gpt-3.5-turbo-16k')
-    if openai.api_key is None:
-        exception_text = 'OpenAI API Key Missing:\n'\
+    try:
+        gpt_responder = OpenAIGPT('gpt-3.5-turbo-16k')
+    except OpenAIError as e:
+        exception_text = 'Error while starting openai:\n'\
+            f'{e}\n\n'\
+            'If the OpenAI API Key is missing:\n'\
             'Please ensure that the API Key is correctly configured in '\
             '`creds.yaml` under `modules.writing_observer.openai_api_key`, '\
             'or alternatively, set it as the `OPENAI_API_KEY` environment '\
@@ -58,15 +58,12 @@ async def process_student_essay(text, prompt, system_prompt, tags):
     We use a closure to allow the system to connect to the memoization KVS.
     '''
 
-    executor = ThreadPoolExecutor()
     copy_tags = tags.copy()
 
     @learning_observer.cache.async_memoization()
     async def gpt(gpt_prompt):
-        loop = asyncio.get_event_loop()
-        partial = functools.partial(gpt_responder.chat_completion, prompt=gpt_prompt, system_prompt=system_prompt)
-        completion = await loop.run_in_executor(executor, partial)
-        return completion["choices"][0]["message"]["content"]
+        completion = await gpt_responder.chat_completion(gpt_prompt, system_prompt)
+        return completion.choices[0].message.content
 
     if len(prompt) == 0:
         output = {
