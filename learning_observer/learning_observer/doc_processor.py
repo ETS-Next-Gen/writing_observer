1721e9b4b290febb6b8d3c81562b65340f88cfa1
Bradley Erickson <bbwe24@gmail.com>
2024-05-07 10:52:19 -0400

    added last mod/process rule

--- learning_observer/learning_observer/doc_processor.py
+++ learning_observer/learning_observer/doc_processor.py
@@ -13,33 +13,72 @@ import learning_observer.stream_analytics.helpers as sa_helpers
 import writing_observer
 import writing_observer.awe_nlp
 import writing_observer.languagetool
+import writing_observer.writing_analysis
 
 '''
 TODO add in desired rules.
 Each rule should be a function that accepts
 a document id.
 Rules we want
-- [ ] document modified since last processing AND
+- [x] document modified since last processing AND
       document not processed in last 5 minutes
 - [ ] teacher visited dashboard recently
 - [ ] events > 2*[events since last processed]
 Start with just the first one
 '''
+async def check_recent_mod_and_not_recent_process(doc_id):
+    '''Determine if the document has been altered since its last
+    processing and check whether it is past a specified cutoff
+    time (5 minutes).
+    '''
+    cutoff = 300 # 5 minutes
+    key = sa_helpers.make_key(
+        process_document,
+        {sa_helpers.EventField('doc_id'): doc_id},
+        sa_helpers.KeyStateType.INTERNAL
+    )
+    doc_info = await KVS[key]
+    if doc_info is None: return True
+
+    key = sa_helpers.make_key(
+        writing_observer.writing_analysis.time_on_task,
+        {sa_helpers.EventField('doc_id'): doc_id, sa_helpers.KeyField.STUDENT: doc_info['student_id']},
+        sa_helpers.KeyStateType.INTERNAL
+    )
+    last_mod = await KVS[key]
+    last_mod = last_mod['saved_ts']
+    last_processed = doc_info['last_processed']
+    now = datetime.datetime.now().timestamp()
+    recently_modified = last_mod > last_processed
+    recently_processed = now - cutoff < last_processed
+    if recently_modified and not recently_processed: return True
+    return False
+
+
 RULES = [
-    lambda x: True
+    check_recent_mod_and_not_recent_process
 ]
 
 KVS = None
+app = None
 
-class mockApp:
+class MockApp:
+    def __init__(self, loop):
+        self.loop = loop
+
     def add_routes(self, *args):
         pass
 
 
+class MockRequest(dict):
+    pass
+
+
 async def start():
     learning_observer.offline.init('creds.yaml')
-    mock_app = mockApp()
-    learning_observer.google.initialize_and_register_routes(mock_app)
+    global app
+    app = MockApp(asyncio.get_event_loop())
+    learning_observer.google.initialize_and_register_routes(app)
     global KVS
     KVS = learning_observer.kvs.KVS()
     while True:
@@ -66,35 +105,44 @@ async def fetch_all_docs():
 
 async def process_documents(docs):
     for doc_id in docs:
-        if check_rules(RULES, doc_id):
+        if await check_rules(RULES, doc_id):
             await process_document(doc_id)
 
 
-def check_rules(rules, doc_id):
+async def check_rules(rules, doc_id):
     '''Determine if a document passes any of the
     provided rules.
     '''
-    return any(rule(doc_id) for rule in rules)
+    for rule in rules:
+        if await rule(doc_id):
+            return True
+    return False
 
 
 async def process_document(doc_id):
-    google_auth = await _fetch_proper_credentials(doc_id)
+    student_id = await _determine_student(doc_id)
+    google_auth = await _fetch_teacher_credentials(student_id)
     doc_text = await _fetch_document_text(doc_id, google_auth)
     if doc_text is None:
         return
-    await _pass_doc_through_analysis(doc_id, doc_text)
+    await _pass_doc_through_analysis(doc_id, doc_text, student_id)
 
 
-async def _fetch_proper_credentials(doc_id):
-    '''Determine which teacher a document belongs to
-    and return the teacher's stored Google auth info
-    '''
+async def _determine_student(doc_id):
     keys = await KVS.keys()
     matching_keys = [k for k in keys if doc_id in k]
     student_specifier = 'STUDENT:'
     students = [k[k.find(student_specifier) + len(student_specifier):] for k in matching_keys]
     # TODO handle more than 1 student per document, but for now just grab the first
     student = students[0]
+    return student
+
+
+async def _fetch_teacher_credentials(student):
+    '''Determine which teacher a document belongs to
+    and return the teacher's stored Google auth info
+    '''
+    keys = await KVS.keys()
     roster_specifier = 'learning_observer.google.roster'
     rosters = [k for k in keys if roster_specifier in k]
     matching_teachers = []
@@ -113,14 +161,6 @@ async def _fetch_proper_credentials(doc_id):
     return creds
 
 
-class MockAppWithLoop:
-    def __init__(self, loop):
-        self.loop = loop
-
-class MockRequest(dict):
-    pass
-
-
 async def _fetch_document_text(doc_id, creds):
     '''Fetch the document text from the appropriate
     Google endpoint.
@@ -132,7 +172,7 @@ async def _fetch_document_text(doc_id, creds):
     mock_request = MockRequest()
     mock_request[learning_observer.constants.AUTH_HEADERS] = creds
     mock_request[learning_observer.constants.USER] = {}
-    mock_request.app = MockAppWithLoop(asyncio.get_event_loop())
+    mock_request.app = app
     runtime = learning_observer.runtime.Runtime(mock_request)
     response = await learning_observer.google.doctext(runtime, documentId=doc_id)
     if 'text' not in response:
@@ -140,7 +180,7 @@ async def _fetch_document_text(doc_id, creds):
     return response['text']
 
 
-async def _pass_doc_through_analysis(doc_id, text):
+async def _pass_doc_through_analysis(doc_id, text, student_id):
     '''Pass the document text through a variety of
     analysis items. This includes,
     - Calculating all AWE components
@@ -152,13 +192,18 @@ async def _pass_doc_through_analysis(doc_id, text):
     lt_output = await writing_observer.languagetool.process_texts(prep_for_lt)
     output = {
         'document_id': doc_id,
+        'student_id': student_id,
         'awe_components': awe_output,
         'languagetool': lt_output,
         'text': text,
-        'last_modified': datetime.datetime.now()
+        'last_processed': datetime.datetime.now().timestamp()
     }
-    print(output)
-    # TODO store output in kvs
+    key = sa_helpers.make_key(
+        process_document,
+        {sa_helpers.EventField('doc_id'): doc_id},
+        sa_helpers.KeyStateType.INTERNAL
+    )
+    await KVS.set(key, output)
 
 
 if __name__ == '__main__':
