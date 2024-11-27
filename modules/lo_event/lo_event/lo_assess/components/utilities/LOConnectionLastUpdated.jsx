5ee16524e862153c8555ac66d163cac52446514f
Bradley Erickson <bbwe24@gmail.com>
2024-11-27 09:05:09 -0500

    moved rendertime to util

--- modules/lo_event/lo_event/lo_assess/components/utilities/LOConnectionLastUpdated.jsx
+++ modules/lo_event/lo_event/lo_assess/components/utilities/LOConnectionLastUpdated.jsx
@@ -12,40 +12,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { LO_CONNECTION_STATUS } from '../constants/LO_CONNECTION_STATUS';
-
-function renderTime (t) {
-  /*
-  Convert seconds to a time string.
-
-  Compact representation.
-    10     ==> 10s
-    125    ==> 2m
-    3600   ==> 1h
-    7601   ==> 2h
-    764450 ==> 8d
-
-  TODO this code exists in `liblo.js` include these functions
-  or migrate them to a utilities file in this LO Event.
-   */
-  const seconds = Math.floor(t) % 60;
-  const minutes = Math.floor(t / 60) % 60;
-  const hours = Math.floor(t / 3600) % 60;
-  const days = Math.floor(t / 3600 / 24);
-
-  if (days > 0) {
-    return String(days) + 'd';
-  }
-  if (hours > 0) {
-    return String(hours) + 'h';
-  }
-  if (minutes > 0) {
-    return String(minutes) + 'm';
-  }
-  if (seconds > 0) {
-    return String(seconds) + 's';
-  }
-  return '-';
-}
+import { renderTime } from '../../../util';
 
 function renderReadableTimeSinceUpdate (timeDifference) {
   if (timeDifference < 5) {
