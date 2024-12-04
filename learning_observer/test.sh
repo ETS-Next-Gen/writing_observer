9259d3b66bc303d3514a4a134eb6137adcefe519
Bradley Erickson <bbwe24@gmail.com>
2024-12-04 15:39:28 -0500

    added test file to learning observer

---
+++ learning_observer/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# learning_observer/test.sh
+echo "================================================="
+echo "Running tests for Learning Observer"
+echo "================================================="
+
+# Modify the commands below to fit your testing needs
+echo "Running doctests"
+# Including the `learning_observer/` path will ignore
+# our `util` and `prototype` directories. We ignore
+# the `main.py` file as it expects us to pass command
+# line arguments.
+pytest --doctest-modules learning_observer/ --ignore=learning_observer/main.py
